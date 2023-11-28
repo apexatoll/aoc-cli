17c6b7b26b69c26c2f6b8cd5262e3510918b309b
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-28 01:20:11 +0000

    Implement Request.build
    - Instantiates a request object with the externally configured session
      token

--- lib/aoc_cli/core/request.rb
+++ lib/aoc_cli/core/request.rb
@@ -7,6 +7,14 @@ module AocCli
         @client = setup_client!(token)
       end
 
+      def self.build
+        token = AocCli.config.session.token
+
+        raise "session token not set" if token.nil?
+
+        new(token:)
+      end
+
       private
 
       def setup_client!(token)
--- spec/aoc_cli/core/request_spec.rb
+++ spec/aoc_cli/core/request_spec.rb
@@ -10,21 +10,51 @@ RSpec.describe AocCli::Core::Request do
     allow(HTTP::Client).to receive(:new).and_return(client)
   end
 
-  describe "#initialize" do
+  shared_examples :sets_up_client do
     let(:expected_cookie) { "session=#{token}" }
 
     it "sets the authentication cookie" do
-      request
+      subject
       expect(HTTP).to have_received(:headers).with(Cookie: expected_cookie).once
     end
 
     it "instantiates an HTTP client" do
-      request
+      subject
       expect(HTTP::Client).to have_received(:new).once
     end
 
     it "stores the client" do
-      expect(request.client).to eq(client)
+      expect(subject.client).to eq(client)
+    end
+  end
+
+  describe "#initialize" do
+    include_examples :sets_up_client
+  end
+
+  describe ".build" do
+    subject(:request) { described_class.build }
+
+    before do
+      allow(AocCli.config.session).to receive(:token).and_return(token)
+    end
+
+    context "when session token is not configured" do
+      let(:token) { nil }
+
+      it "raises an error" do
+        expect { request }.to raise_error("session token not set")
+      end
+    end
+
+    context "when session token is configured" do
+      let(:token) { "123456abcdef" }
+
+      it "does not raises any errors" do
+        expect { request }.not_to raise_error
+      end
+
+      include_examples :sets_up_client
     end
   end
 end
