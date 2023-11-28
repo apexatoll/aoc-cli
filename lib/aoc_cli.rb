a8cb3c5f2a75efff1a7c9dcd1c30c677d4746706
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-28 00:01:13 +0000

    Configure databases

--- lib/aoc_cli.rb
+++ lib/aoc_cli.rb
@@ -7,5 +7,15 @@ module AocCli
 
   config_path "spec/aoc.yml"
 
+  configure do |config|
+    config.database.adaptor = :sqlite
+    config.database.path = File.expand_path("~/.local/share/aoc/aoc.sqlite3")
+    config.database.migration_path = "db/migrate"
+  end
+
+  configure env: :test do |config|
+    config.database.path = "db/test.sqlite3"
+  end
+
   apply_config!
 end
