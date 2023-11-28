c808d3206aa5ca397f139e0b3d2544094676e93c
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-28 21:28:08 +0000

    Install VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "aoc_cli"
 require "webmock/rspec"
+require "vcr"
 
 RSpec.configure do |config|
   Kernel.srand(config.seed)
@@ -13,4 +14,9 @@ RSpec.configure do |config|
   config.order = :random
 
   config.default_formatter = :doc if config.files_to_run.one?
+
+  VCR.configure do |vcr|
+    vcr.hook_into :webmock
+    vcr.cassette_library_dir = "spec/fixtures"
+  end
 end
