8aa26fb6aeec0ddeaebf2b5770e7451500722141
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-29 19:51:44 +0000

    Automatically require files in spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require "aoc_cli"
 RSpec.configure do |config|
   Kernel.srand(config.seed)
 
+  Dir[File.join(__dir__, "support/**/*.rb")].each { |file| require file }
+
   config.disable_monkey_patching!
 
   config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
