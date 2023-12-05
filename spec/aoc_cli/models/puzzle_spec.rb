a56fd4f8c1c3c8bec32e3ff93198005277a5ee5f
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-05 01:19:01 +0000

    Extract day validations into shared_examples

--- spec/aoc_cli/models/puzzle_spec.rb
+++ spec/aoc_cli/models/puzzle_spec.rb
@@ -24,41 +24,7 @@ RSpec.describe AocCli::Puzzle do
     end
 
     describe ":day" do
-      context "when nil" do
-        let(:day) { nil }
-
-        include_examples :invalid, errors: ["Day can't be blank"]
-      end
-
-      context "when not an integer" do
-        let(:day) { :day }
-
-        include_examples :invalid, errors: ["Day is not an integer"]
-      end
-
-      context "when negative" do
-        let(:day) { -1 }
-
-        include_examples :invalid, errors: ["Day is too small"]
-      end
-
-      context "when too low" do
-        let(:day) { 0 }
-
-        include_examples :invalid, errors: ["Day is too small"]
-      end
-
-      context "when too high" do
-        let(:day) { 26 }
-
-        include_examples :invalid, errors: ["Day is too large"]
-      end
-
-      context "when valid" do
-        let(:day) { 1 }
-
-        include_examples :valid
-      end
+      include_examples :validates_puzzle_day
     end
 
     describe ":contents" do
--- spec/support/examples/validations.rb
+++ spec/support/examples/validations.rb
@@ -53,3 +53,41 @@ RSpec.shared_examples :validates_event_year do
     include_examples :valid
   end
 end
+
+RSpec.shared_examples :validates_puzzle_day do
+  context "when nil" do
+    let(:day) { nil }
+
+    include_examples :invalid, errors: ["Day can't be blank"]
+  end
+
+  context "when not an integer" do
+    let(:day) { :day }
+
+    include_examples :invalid, errors: ["Day is not an integer"]
+  end
+
+  context "when negative" do
+    let(:day) { -1 }
+
+    include_examples :invalid, errors: ["Day is too small"]
+  end
+
+  context "when too low" do
+    let(:day) { 0 }
+
+    include_examples :invalid, errors: ["Day is too small"]
+  end
+
+  context "when too high" do
+    let(:day) { 26 }
+
+    include_examples :invalid, errors: ["Day is too large"]
+  end
+
+  context "when valid" do
+    let(:day) { 1 }
+
+    include_examples :valid
+  end
+end
