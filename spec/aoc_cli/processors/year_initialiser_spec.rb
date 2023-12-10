702b247f0ee50ac043ecfcf143670eb8ac33f05f
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-10 13:10:52 +0000

    Implement YearInitialiser#run

--- spec/aoc_cli/processors/year_initialiser_spec.rb
+++ spec/aoc_cli/processors/year_initialiser_spec.rb
@@ -3,9 +3,11 @@ RSpec.describe AocCli::Processors::YearInitialiser, :with_temp_dir do
 
   let(:attributes) { { year:, dir: } }
 
-  describe "validations" do
-    let(:dir) { temp_dir.to_s }
+  let(:year) { 2020 }
 
+  let(:dir) { temp_dir.to_s }
+
+  describe "validations" do
     context "when year is nil" do
       let(:year) { nil }
 
@@ -58,6 +60,170 @@ RSpec.describe AocCli::Processors::YearInitialiser, :with_temp_dir do
 
         context "and year path does not already exist" do
           include_examples :valid
+        end
+      end
+    end
+  end
+
+  describe "#run" do
+    subject(:run) { year_initialiser.run }
+
+    let(:year_dir) { temp_path(year.to_s).to_s }
+
+    before do
+      allow(AocCli::Processors::StatsRefresher).to receive(:run)
+    end
+
+    context "when invalid" do
+      let(:year) { 2014 }
+
+      it "does not create an Event record" do
+        expect { run }.not_to change { AocCli::Event.count }
+      end
+
+      it "does not fetch the stats" do
+        run
+        expect(AocCli::Processors::StatsRefresher).not_to have_received(:run)
+      end
+
+      it "does not create a Location record" do
+        expect { run }.not_to change { AocCli::Location.count }
+      end
+
+      it "does not create the year directory" do
+        expect { run }.not_to create_temp_dir(year_dir)
+      end
+    end
+
+    context "when valid" do
+      let(:year) { 2015 }
+
+      context "and event does not already exist" do
+        let(:event)    { AocCli::Event.last }
+        let(:location) { AocCli::Location.last }
+
+        it "creates an Event record" do
+          expect { run }.to change { AocCli::Event.count }.by(1)
+        end
+
+        it "sets the expected Event attributes" do
+          run
+          expect(event).to have_attributes(year:)
+        end
+
+        it "fetches the stats" do
+          run
+
+          expect(AocCli::Processors::StatsRefresher)
+            .to have_received(:run)
+            .with(year:)
+            .once
+        end
+
+        it "creates a Location record" do
+          expect { run }.to change { AocCli::Location.count }.by(1)
+        end
+
+        it "sets the expected Location attributes" do
+          run
+
+          expect(location).to have_attributes(
+            path: year_dir, resource: event
+          )
+        end
+
+        it "creates the year directory" do
+          expect { run }.to create_temp_dir(year_dir)
+        end
+
+        it "returns the Event record" do
+          expect(run).to eq(event)
+        end
+      end
+
+      context "and event already exists" do
+        let!(:event) { create(:event, year:) }
+
+        context "and event location is set" do
+          let!(:location) { create(:location, :year_dir, event:) }
+
+          it "does not create an Event record" do
+            expect { run }.not_to change { AocCli::Event.count }
+          end
+
+          it "does not update the Event attributes" do
+            expect { run }.not_to change { event.reload.values }
+          end
+
+          it "fetches the stats" do
+            run
+
+            expect(AocCli::Processors::StatsRefresher)
+              .to have_received(:run)
+              .with(year:)
+              .once
+          end
+
+          it "does not create a Location record" do
+            expect { run }.not_to change { AocCli::Location.count }
+          end
+
+          it "updates the expected Location attributes" do
+            run
+
+            expect(location.reload).to have_attributes(
+              path: year_dir, resource: event
+            )
+          end
+
+          it "creates the year directory" do
+            expect { run }.to create_temp_dir(year_dir)
+          end
+
+          it "returns the Event record" do
+            expect(run).to eq(event)
+          end
+        end
+
+        context "and event location is not already set" do
+          let(:location) { AocCli::Location.last }
+
+          it "does not create an Event record" do
+            expect { run }.not_to change { AocCli::Event.count }
+          end
+
+          it "does not update the Event attributes" do
+            expect { run }.not_to change { event.reload.values }
+          end
+
+          it "fetches the stats" do
+            run
+
+            expect(AocCli::Processors::StatsRefresher)
+              .to have_received(:run)
+              .with(year:)
+              .once
+          end
+
+          it "creates a Location record" do
+            expect { run }.to change { AocCli::Location.count }.by(1)
+          end
+
+          it "sets the expected Location attributes" do
+            run
+
+            expect(location).to have_attributes(
+              path: year_dir, resource: event
+            )
+          end
+
+          it "creates the year directory" do
+            expect { run }.to create_temp_dir(year_dir)
+          end
+
+          it "returns the Event record" do
+            expect(run).to eq(event)
+          end
         end
       end
     end
