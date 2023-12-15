33f23f8b7dfe73c6362aa354ffa3b04f28532c6b
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-15 22:51:23 +0000

    Implement ErrorsHelper#render_errors!

--- lib/aoc_cli/helpers/errors_helper.rb
+++ lib/aoc_cli/helpers/errors_helper.rb
@@ -1,6 +1,12 @@
 module AocCli
   module Helpers
     module ErrorsHelper
+      def render_errors!(*errors)
+        Components::ErrorsComponent.new(*errors).render
+
+        false
+      end
+
       def render_model_errors!(model)
         Components::ErrorsComponent.from_model(model).render
 
--- spec/aoc_cli/helpers/errors_helper_spec.rb
+++ spec/aoc_cli/helpers/errors_helper_spec.rb
@@ -6,9 +6,34 @@ RSpec.describe AocCli::Helpers::ErrorsHelper do
   end
 
   before do
-    allow(AocCli::Components::ErrorsComponent)
-      .to receive(:from_model)
-      .and_return(errors_component)
+    allow(AocCli::Components::ErrorsComponent).to receive_messages(
+      new: errors_component,
+      from_model: errors_component
+    )
+  end
+
+  describe "#render_errors!" do
+    subject(:render_errors!) { target.render_errors!(*errors) }
+
+    let(:errors) { ["Foo Error", "Bar Error"] }
+
+    it "builds an errors component" do
+      render_errors!
+
+      expect(AocCli::Components::ErrorsComponent)
+        .to have_received(:new)
+        .with(*errors)
+        .once
+    end
+
+    it "renders the component" do
+      render_errors!
+      expect(errors_component).to have_received(:render).once
+    end
+
+    it "returns false" do
+      expect(render_errors!).to be(false)
+    end
   end
 
   describe "#render_model_errors!" do
