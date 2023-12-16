b7ff821e88e6f07fc486469740a2165e038a22e7
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-16 21:26:36 +0000

    Update repository spec to use fixture helpers

--- spec/aoc_cli/core/repository_spec.rb
+++ spec/aoc_cli/core/repository_spec.rb
@@ -3,20 +3,6 @@ RSpec.describe AocCli::Core::Repository do
     File.join(described_class::HOST, path)
   end
 
-  shared_examples :gets_resource do |options|
-    let(:path)     { options[:path] }
-    let(:resource) { options[:resource] }
-
-    it "makes the expected GET request" do
-      subject
-      assert_requested(:get, path_to_url(path))
-    end
-
-    it "returns the expected resource" do
-      expect(subject).to eq(fixture(resource))
-    end
-  end
-
   describe ".get_stats" do
     subject(:stats) { described_class.get_stats(year:) }
 
@@ -69,85 +55,78 @@ RSpec.describe AocCli::Core::Repository do
 
     around { |spec| use_puzzle_cassette(year:, day:) { spec.run } }
 
+    shared_examples :gets_puzzle do
+      it "makes the expected GET request" do
+        puzzle
+        assert_requested(:get, path_to_url("#{year}/day/#{day}"))
+      end
+
+      it "returns the expected resource" do
+        expect(puzzle).to eq(puzzle_fixture(year:, day:))
+      end
+    end
+
     describe "2015" do
       let(:year) { 2015 }
       let(:day)  { 3 }
 
-      include_examples :gets_resource,
-                       path: "2015/day/3",
-                       resource: "puzzle-2015-03.md"
+      include_examples :gets_puzzle
     end
 
     describe "2016" do
       let(:year) { 2016 }
       let(:day)  { 8 }
 
-      include_examples :gets_resource,
-                       path: "2016/day/8",
-                       resource: "puzzle-2016-08.md"
+      include_examples :gets_puzzle
     end
 
     describe "2017" do
       let(:year) { 2017 }
       let(:day)  { 15 }
 
-      include_examples :gets_resource,
-                       path: "2017/day/15",
-                       resource: "puzzle-2017-15.md"
+      include_examples :gets_puzzle
     end
 
     describe "2018" do
       let(:year) { 2018 }
       let(:day)  { 19 }
 
-      include_examples :gets_resource,
-                       path: "2018/day/19",
-                       resource: "puzzle-2018-19.md"
+      include_examples :gets_puzzle
     end
 
     describe "2019" do
       let(:year) { 2019 }
       let(:day)  { 1 }
 
-      include_examples :gets_resource,
-                       path: "2019/day/1",
-                       resource: "puzzle-2019-01.md"
+      include_examples :gets_puzzle
     end
 
     describe "2020" do
       let(:year) { 2020 }
       let(:day)  { 23 }
 
-      include_examples :gets_resource,
-                       path: "2020/day/23",
-                       resource: "puzzle-2020-23.md"
+      include_examples :gets_puzzle
     end
 
     describe "2021" do
       let(:year) { 2021 }
       let(:day)  { 20 }
 
-      include_examples :gets_resource,
-                       path: "2021/day/20",
-                       resource: "puzzle-2021-20.md"
+      include_examples :gets_puzzle
     end
 
     describe "2022" do
       let(:year) { 2022 }
       let(:day)  { 8 }
 
-      include_examples :gets_resource,
-                       path: "2022/day/8",
-                       resource: "puzzle-2022-08.md"
+      include_examples :gets_puzzle
     end
 
     describe "2023" do
       let(:year) { 2023 }
       let(:day)  { 1 }
 
-      include_examples :gets_resource,
-                       path: "2023/day/1",
-                       resource: "puzzle-2023-01.md"
+      include_examples :gets_puzzle
     end
   end
 
@@ -156,85 +135,78 @@ RSpec.describe AocCli::Core::Repository do
 
     around { |spec| use_input_cassette(year:, day:) { spec.run } }
 
+    shared_examples :gets_input do
+      it "makes the expected GET request" do
+        input
+        assert_requested(:get, path_to_url("#{year}/day/#{day}/input"))
+      end
+
+      it "returns the expected resource" do
+        expect(input).to eq(input_fixture(year:, day:))
+      end
+    end
+
     describe "2015" do
       let(:year) { 2015 }
       let(:day)  { 3 }
 
-      include_examples :gets_resource,
-                       path: "2015/day/3/input",
-                       resource: "input-2015-03"
+      include_examples :gets_input
     end
 
     describe "2016" do
       let(:year) { 2016 }
       let(:day)  { 8 }
 
-      include_examples :gets_resource,
-                       path: "2016/day/8/input",
-                       resource: "input-2016-08"
+      include_examples :gets_input
     end
 
     describe "2017" do
       let(:year) { 2017 }
       let(:day)  { 15 }
 
-      include_examples :gets_resource,
-                       path: "2017/day/15/input",
-                       resource: "input-2017-15"
+      include_examples :gets_input
     end
 
     describe "2018" do
       let(:year) { 2018 }
       let(:day)  { 19 }
 
-      include_examples :gets_resource,
-                       path: "2018/day/19/input",
-                       resource: "input-2018-19"
+      include_examples :gets_input
     end
 
     describe "2019" do
       let(:year) { 2019 }
       let(:day)  { 1 }
 
-      include_examples :gets_resource,
-                       path: "2019/day/1/input",
-                       resource: "input-2019-01"
+      include_examples :gets_input
     end
 
     describe "2020" do
       let(:year) { 2020 }
       let(:day)  { 23 }
 
-      include_examples :gets_resource,
-                       path: "2020/day/23/input",
-                       resource: "input-2020-23"
+      include_examples :gets_input
     end
 
     describe "2021" do
       let(:year) { 2021 }
       let(:day)  { 20 }
 
-      include_examples :gets_resource,
-                       path: "2021/day/20/input",
-                       resource: "input-2021-20"
+      include_examples :gets_input
     end
 
     describe "2022" do
       let(:year) { 2022 }
       let(:day)  { 8 }
 
-      include_examples :gets_resource,
-                       path: "2022/day/8/input",
-                       resource: "input-2022-08"
+      include_examples :gets_input
     end
 
     describe "2023" do
       let(:year) { 2023 }
       let(:day) { 1 }
 
-      include_examples :gets_resource,
-                       path: "2023/day/1/input",
-                       resource: "input-2023-01"
+      include_examples :gets_input
     end
   end
 
