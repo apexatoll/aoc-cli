e2dc5c421e47107de80cf0b9d2770ad7984ee2f7
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-22 10:41:40 +0000

    Update :create_model matcher to handle with_attributes

--- spec/support/matchers/create_model.rb
+++ spec/support/matchers/create_model.rb
@@ -5,7 +5,19 @@ module Matchers
     supports_block_expectations
 
     match do |action|
-      expect { action.call }.to change { model_class.count }.by(1)
+      count_before = model_class.count
+      action.call
+      count_after = model_class.count
+
+      created = count_after - count_before == 1
+
+      return created unless @attributes
+
+      expect(model_class.last).to have_attributes(**@attributes)
+    end
+
+    chain :with_attributes do |attributes|
+      @attributes = attributes
     end
   end
 end
