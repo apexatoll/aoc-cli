ebc6c35f08645bb45442f732ab81b60b07d1414d
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-12-23 03:01:30 +0000

    Implement :request matcher

---
+++ spec/support/matchers/request.rb
@@ -0,0 +1,21 @@
+module Matchers
+  extend RSpec::Matchers::DSL
+
+  matcher :request do |url|
+    supports_block_expectations
+
+    match do |action|
+      @method ||= :get
+
+      assert_not_requested(@method, url)
+      action.call
+      assert_requested(@method, url)
+
+      true
+    end
+
+    chain :via do |method|
+      @method = method
+    end
+  end
+end
