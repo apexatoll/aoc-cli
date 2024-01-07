c8a2d8c866fbcf01a7c9b2f24e2a09db05b5945e
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-01-07 19:04:40 +0000

    Handle processor errors for all controllers

--- lib/aoc_cli/controllers/application_controller.rb
+++ lib/aoc_cli/controllers/application_controller.rb
@@ -2,5 +2,11 @@ module AocCli
   class ApplicationController < Kangaru::Controller
     include Helpers::ErrorHelper
     include Helpers::LocationHelper
+
+    def execute
+      super
+    rescue Core::Processor::Error => e
+      render_model_errors!(e.processor)
+    end
   end
 end
