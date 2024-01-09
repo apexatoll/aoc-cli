e271594dd79be7a92c56bc2153ec235d84e19fcf
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-01-09 17:43:31 +0000

    Implement /event/attach action

--- lib/aoc_cli/controllers/event_controller.rb
+++ lib/aoc_cli/controllers/event_controller.rb
@@ -9,10 +9,27 @@ module AocCli
       )
     end
 
+    def attach
+      return render_error!("Event does not exist") if queried_event.nil?
+
+      @source = queried_event.location.path
+
+      @target = Processors::ResourceAttacher.run!(
+        resource: queried_event,
+        path: params[:dir] || current_path
+      ).path
+    end
+
     def progress
       return unless ensure_in_aoc_dir!
 
       Components::ProgressTable.new(event: current_event || raise).render
+    end
+
+    private
+
+    def queried_event
+      @queried_event ||= Event.first(year: target_id)
     end
   end
 end
---
+++ spec/aoc_cli/controllers/event/attach_spec.rb
@@ -0,0 +1,90 @@
+RSpec.describe "/event/attach/:year", :with_temp_dir do
+  subject(:make_request) { resolve path, params: }
+
+  let(:path) { ["/event/attach", year].compact.join("/") }
+
+  let(:params) { { dir: } }
+
+  let(:dir) { nil }
+
+  shared_examples :does_not_attach_event do |options|
+    let(:errors) { options[:errors] }
+
+    it "renders the expected errors" do
+      expect { make_request }.to render_errors(*errors)
+    end
+  end
+
+  shared_examples :attaches_event do
+    it "updates the event location" do
+      expect { make_request }
+        .to change { event.location.reload.path }
+        .from(source_path)
+        .to(target_path)
+    end
+
+    it "does not render any errors" do
+      expect { make_request }.not_to render_errors
+    end
+
+    it "renders the expected response" do
+      expect { make_request }.to output(<<~TEXT).to_stdout
+        Event (#{year}) location updated
+          from #{source_path}
+          to   #{target_path}
+      TEXT
+    end
+  end
+
+  context "when year is nil" do
+    let(:year) { nil }
+
+    include_examples :does_not_attach_event, errors: [
+      "Event does not exist"
+    ]
+  end
+
+  context "when year is valid" do
+    let(:year) { 2015 }
+
+    context "and year event has not been initialised" do
+      include_examples :does_not_attach_event, errors: [
+        "Event does not exist"
+      ]
+    end
+
+    context "and year event has been initialised" do
+      let!(:event) do
+        create(:event, :with_location, year:, path: source_path)
+      end
+
+      let(:source_path) { temp_path("source").tap(&:mkdir).to_s }
+
+      context "and no dir is specified" do
+        let(:dir) { nil }
+
+        include_examples :attaches_event do
+          let(:target_path) { temp_dir }
+        end
+      end
+
+      context "and dir is specified" do
+        let(:dir) { temp_path("target").to_s }
+
+        context "and specified dir does not exist" do
+          include_examples :does_not_attach_event, errors: [
+            "Path does not exist"
+          ]
+        end
+
+        context "and specified dir exists" do
+          before { Dir.mkdir(dir) }
+
+          include_examples :attaches_event do
+            let(:target_path) { dir }
+          end
+        end
+      end
+    end
+  end
+end
