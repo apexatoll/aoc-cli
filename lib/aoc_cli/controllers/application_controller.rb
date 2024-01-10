51fa5d66a5774a564a715adbbf25935d851c6c49
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-01-10 20:17:10 +0000

    Update controllers to redirect to help if --help passed

--- lib/aoc_cli/controllers/application_controller.rb
+++ lib/aoc_cli/controllers/application_controller.rb
@@ -4,9 +4,22 @@ module AocCli
     include Helpers::LocationHelper
 
     def execute
+      return handle_help_param! if params[:help]
+
       super
     rescue Core::Processor::Error => e
       render_model_errors!(e.processor)
+    end
+
+    private
+
+    def handle_help_param!
+      params.delete(:help)
+
+      path = File.join("/help", request.path)
+      request = Kangaru::Request.new(path:, params:)
+
+      Kangaru.application!.router.resolve(request)
     end
   end
 end
--- spec/aoc_cli/controllers/application_controller_spec.rb
+++ spec/aoc_cli/controllers/application_controller_spec.rb
@@ -53,5 +53,57 @@ RSpec.describe AocCli::ApplicationController do
         end
       end
     end
+
+    describe "handling --help flag" do
+      subject(:make_request) { resolve "/test/foobar", params: }
+
+      let(:params) { { help:, param: }.compact }
+
+      let(:param) { nil }
+
+      let(:controller_class) do
+        Class.new(described_class) { def foobar; end }
+      end
+
+      let(:help_controller_class) do
+        Class.new(described_class) { def foobar; end }
+      end
+
+      before do
+        stub_const "AocCli::Help::TestController", help_controller_class
+      end
+
+      context "when help flag is not passed" do
+        let(:help) { nil }
+
+        it "does not redirect to the help controller action" do
+          expect { make_request }.not_to redirect_to("/help/test/foobar")
+        end
+      end
+
+      context "when help flag is passed" do
+        let(:help) { true }
+
+        context "and no other params are specified" do
+          let(:param) { nil }
+
+          it "redirects to the help controller action without params" do
+            expect { make_request }
+              .to redirect_to("/help/test/foobar")
+              .with_params({})
+          end
+        end
+
+        context "and other params are specified" do
+          let(:param) { "value" }
+
+          it "redirects to the help controller action with param" do
+            expect { make_request }
+              .to redirect_to("/help/test/foobar")
+              .with_params(param:)
+          end
+        end
+      end
+    end
   end
 end
